7c1f22d4348e28644999841bf5475375c835546b
y12studio <y12studio@gmail.com>
2015-07-19 09:24:09 +0800

    bitcore-node regetest mode

--- node/bitcore-node/bcnode/app.js
+++ node/bitcore-node/bcnode/app.js
@@ -23,31 +23,30 @@ bitcore.Networks.add({
     dnsSeeds: [],
 });
 
-function mainfun() {
-    if (require.main === module) {
-        var config = require('config');
-        var nodeConfig = config.get('BitcoreHTTP.BitcoreNode');
-        var httpConfig = config.get('BitcoreHTTP');
-        var network = nodeConfig.network;
-        console.log('Starting bitcore-node-http', network, 'network');
-        bitcore.Networks.defaultNetwork = bitcore.Networks.get(network);
-        var node = BitcoreNode.create(nodeConfig);
-        node.on('error', function(err) {
-            if (err.code === 'ECONNREFUSED') {
-                console.log('Connection to bitcoind failed');
-            } else {
-                console.log('Error: ', err);
-            }
+if (require.main === module) {
+    var config = require('config');
+    var nodeConfig = config.get('BitcoreHTTP.BitcoreNode');
+    var httpConfig = config.get('BitcoreHTTP');
+    var network = nodeConfig.network;
+    console.log('Starting bitcore-node-http', network, 'network');
+    bitcore.Networks.defaultNetwork = bitcore.Networks.get(network);
+    //
+    var node = BitcoreNode.create(nodeConfig);
+    node.on('error', function(err) {
+        if (err.code === 'ECONNREFUSED') {
+            console.log('Connection to bitcoind failed');
+        } else {
+            console.log('Error: ', err);
+        }
+    });
+    var http = new BitcoreHTTP(node, httpConfig);
+    http.start()
+        .catch(function(err) {
+            http.stop();
+            throw err;
         });
-        var http = new BitcoreHTTP(node, httpConfig);
-        http.start()
-            .catch(function(err) {
-                http.stop();
-                throw err;
-            });
-    }
+}
 
-};
-
-setTimeout(mainfun, 10000);
+// setTimeout(mainfun, 10000);
+// mainfun();
 console.log('Server running.');
