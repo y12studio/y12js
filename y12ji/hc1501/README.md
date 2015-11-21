aa24319bec5452d3aea51ba7b4ad621379991a14
y12studio <y12studio@gmail.com>
2015-11-21 18:08:14 +0800

    bitcore-lib, bip38, bundle, browserify

---
+++ y12ji/hc1501/README.md
@@ -0,0 +1,6 @@
+build hc1501
+```
+$ npm install
+$ browserify app.js -s hc1501 -o hc1501.js
+$ minify hc1501.js
+```
---
+++ y12ji/hc1501/app.js
@@ -0,0 +1,6 @@
+
+var hc1501 = {
+  bip38: require('bip38'),
+  bitcorelib: require('bitcore-lib')
+}
+module.exports = hc1501
