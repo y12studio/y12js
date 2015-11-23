4af6c4ec6a39b8e1bd5c37ecb247677d488e669b
y12studio <y12studio@gmail.com>
2015-11-23 13:16:57 +0800

    add bitcore-mnemonic lib to hc1501 ver 0.1.1

--- y12ji/hc1501/app.js
+++ y12ji/hc1501/app.js
@@ -1,6 +1,7 @@
 
 var hc1501 = {
   bip38: require('bip38'),
-  bitcorelib: require('bitcore-lib')
+  bitcorelib: require('bitcore-lib'),
+  bitcoremnemonic : require('bitcore-mnemonic')
 }
 module.exports = hc1501
