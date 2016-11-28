6821e52e0f37657d65d3cf174641b1ea161d7653
y12studio <y12studio@gmail.com>
2016-11-28 17:29:39 +0800

    y12certs:buildCpTargetHash

--- y12ji/y12certs/app.js
+++ y12ji/y12certs/app.js
@@ -1,4 +1,5 @@
 var bitcorelib = require('bitcore-lib')
+var Hash = bitcorelib.crypto.Hash
 var _ = require('lodash')
 var stringify = require('json-stable-stringify')
 var merkletools = require('merkle-tools')
@@ -18,8 +19,75 @@ yapp.cptemplate = {
     }]
 }
 
+yapp.bctemplate = {
+    "document": {
+        "signature": "IBWesnPz0m+4SfrhLsRYjF4GmwaA4QmAxAEpIBqVQIrPTNdXO3nA01ZGYF7ALfNSXgor8bfZxKyJDxnqfuj5NOM=",
+        "certificate": {
+            "language": "en-US",
+            "type": "Certificate",
+            "image": "data:image/png;base64",
+            "issuer": {
+                "url": "http://www.blockcerts.org/mockissuer/",
+                "type": "Issuer",
+                "image": "data:image/png;base64,",
+                "name": "Game of thrones issuer",
+                "email": "fakeEmail@gamoeofthronesxyz.org",
+                "id": "http://www.blockcerts.org/mockissuer/issuer/got-issuer.json"
+            },
+            "id": "http://certificates.gamoeofthronesxyz.org/criteria/2016/08/got.json",
+            "description": "This certifies that the named character is an official Game of Thrones character.",
+            "name": "Game of Thrones Character"
+        },
+        "recipient": {
+            "publicKey": "mgCNaPM3TFhh8Yn6U6VcEM9jWLhQbizy1x",
+            "revocationKey": "mhjWi9Y3VhjcPmg2hcdjWBFZFTHtKuWFL2",
+            "hashed": false,
+            "givenName": "Arya",
+            "familyName": "Stark",
+            "identity": "aryaxyz@starkxyz.com",
+            "type": "email"
+        },
+        "type": "CertificateDocument",
+        "@context": "https://w3id.org/blockcerts/v1",
+        "verify": {
+            "attribute-signed": "uid",
+            "type": "ECDSA(secp256k1)",
+            "signer": "http://www.blockcerts.org/mockissuer/keys/got_public_key.asc"
+        },
+        "assertion": {
+            "image:signature": "data:image/png;base64,",
+            "type": "Assertion",
+            "uid": "f813349f-1385-487f-8d89-38a092411fa5",
+            "id": "http://certificates.gamoeofthronesxyz.org/f813349f-1385-487f-8d89-38a092411fa5",
+            "issuedOn": "2016-09-29"
+        }
+    },
+    "type": "BlockchainCertificate",
+    "receipt": {},
+    "@context": "https://w3id.org/blockcerts/v1"
+}
+
 yapp.newReceipt = function() {
     return _.cloneDeep(yapp.cptemplate)
+}
+
+yapp.newBlockcert = function() {
+    return _.cloneDeep(yapp.bctemplate)
+}
+
+yapp.sha256 = function(targetStr) {
+    return Hash.sha256(Buffer.from(targetStr)).toString('hex')
+}
+
+yapp.buildCpTargetHash = function(bcObj, cb) {
+    yapp.normalize(bcObj.document, function(err, res) {
+        if (err) {
+            cb(err)
+        } else {
+            var hash = yapp.sha256(res)
+            cb(null, hash)
+        }
+    })
 }
 
 //
--- y12ji/y12certs/test/testApp.js
+++ y12ji/y12certs/test/testApp.js
@@ -5,6 +5,8 @@ var _ = app._
 var stringify = app.stringify
 var bitcorelib = app.bitcorelib
 
+var blockCertsSample = require('./sample_signed_cert-1.2.json')
+
 beforeEach(function() {
     //do something before testing
 });
@@ -24,17 +26,43 @@ describe('App', function() {
             r1.targetHash = 'fd3f0550fd1164f463d3e57b7bb6834872ada68501102cec6ce93cdbe7a17404'
             r2.targetHash = '7c9dfce9ca4684321e4851ce263ac730652d1897e5041196ffe989790317679c'
             r3.targetHash = 'cb4c46e8ebbc1d646f07a8ff00561a92c83cc1f40ea27d85b1e90c3f858b3815'
-            var rarr = yapp.buildProof([r1,r2,r3])
+            var rarr = yapp.buildProof([r1, r2, r3])
             console.log(JSON.stringify(rarr, null, 4))
+        })
+
+        it('newBlockcert', function() {
+            var bc1 = yapp.newBlockcert()
+            var bc2 = yapp.newBlockcert()
+            assert.isTrue(bc1!=bc2)
+        })
+
+        it('sha256', function() {
+            var hash = yapp.sha256('hello world')
+            assert.equal('b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9', hash)
         })
 
         it('buildProof', function(done) {
             this.timeout(5000)
             var r1 = yapp.newReceipt()
-            yapp.normalize(r1,function(err,res){
+            yapp.normalize(r1, function(err, res) {
                 console.log(res)
+                console.log(typeof res)
+                assert.isString(res)
                 done()
             })
         })
+
+        it('buildCpTargetHash', function(done) {
+            this.timeout(5000)
+            var bc = blockCertsSample
+            yapp.buildCpTargetHash(bc, function(err, res) {
+                console.log(res)
+                console.log(typeof res)
+                assert.isString(res)
+                assert.equal(bc.receipt.targetHash, res)
+                done()
+            })
+        })
+
     })
 })
