dbdea1d596b5a880c654300e2b0e2e8eb8faab53
JOYE LIN <y12studio@gmail.com>
2018-02-24 17:32:14 +0800

    new project vue-inline-bitcoinjs and bip39 for one html file output

---
+++ vue-inline-bitcoinjs/src/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <div id="app">
+    <h1>{{ msg }}</h1>
+    <h2>{{ key }}</h2>
+    <b-jumbotron header="BIP39" :lead="mnemonic" >
+  <p>{{mnemonic}}</p>
+  <b-btn variant="primary" href="#">More Info</b-btn>
+</b-jumbotron>
+  <div>
+    <b-card title="Card title"
+            sub-title="Card subtitle">
+        <p class="card-text">
+            Some quick example text to build on the <em>card title</em> and make up the bulk of the card's content.
+        </p>
+         <b-button disabled variant="success">Disabled</b-button>
+  <b-button variant="success">Not Disabled</b-button>
+        <a href="#"
+           class="card-link">Card link</a>
+        <b-link href="#"
+                class="card-link">Another link</b-link>
+    </b-card>
+</div>
+  </div>
+</template>
+
+<script>
+const bitcoin = require('bitcoinjs-lib')
+const bip39 = require('bip39')
+let key = bitcoin.ECPair.makeRandom().toWIF();
+let mnemonic = bip39.generateMnemonic();
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Vue.js App ....',
+      key: key,
+      mnemonic: mnemonic
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ vue-inline-bitcoinjs/webpack.config.js
@@ -0,0 +1,87 @@
+var path = require('path')
+var webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require("extract-text-webpack-plugin")
+const StyleExtHtmlWebpackPlugin = require('style-ext-html-webpack-plugin')
+const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin')
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'build.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+        }),
+      }, {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+            // other vue-loader options go here
+        }
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]?[hash]'
+        }
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    },
+    extensions: ['*', '.js', '.vue', '.json']
+  },
+  plugins: [new ExtractTextPlugin("main.css")],
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true,
+    overlay: true
+  },
+  performance: {
+    hints: false
+  },
+  devtool: '#eval-source-map'
+}
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map'
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html.tpl'
+    }),
+    new StyleExtHtmlWebpackPlugin(),
+    new ScriptExtHtmlWebpackPlugin({ inline: 'build' }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ])
+}
